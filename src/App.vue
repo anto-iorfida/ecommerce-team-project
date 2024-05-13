b0df03dad137a35b87f5707044a6919f52afe7e8
anto-iorfida <antonioiorfida@hotmail.com>
2024-05-13 17:46:02 +0200

    feat add DiscoverProduct

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script>
-
+  import DiscoverProduct from './components/DiscoverProduct.vue';
+ 
+  export default {
+    components: {
+      DiscoverProduct,
+    }
+  }
 </script>
 
 <template>
-  
+<DiscoverProduct></DiscoverProduct>
 </template>
 
 <style lang="scss" >
-
+@use './style/general.scss' as *;
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue'
 import App from './App.vue'
-import * as bootstrap from 'bootstrap'
 createApp(App).mount('#app')
+
+
+
