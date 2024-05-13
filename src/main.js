80c71022f630842ae4d5f5edde304d5717c43332
Kapo <paykapo4@gmail.com>
2024-05-13 18:05:25 +0200

    fix push errors

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue'
 import App from './App.vue'
-<<<<<<< HEAD
-=======
 import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> d1e1d012ab3b482ec58165de1cf517b213f7363a
 createApp(App).mount('#app')
 
 
